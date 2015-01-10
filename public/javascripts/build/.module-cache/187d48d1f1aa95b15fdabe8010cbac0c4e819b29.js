65cfd465a06972ca43e6309ef9b2c86b17d15e05
Josh Finnie <josh.finnie@trackmaven.com>
2015-01-10 12:16:17 -0500

    adding .module-cache to gitignore.

--- public/javascripts/build/.module-cache/187d48d1f1aa95b15fdabe8010cbac0c4e819b29.js
+++
@@ -1,5 +0,0 @@
-React.render(
-  React.createElement("h1", null, "Hello, world from React.js!"),
-  document.getElementById('example')
-);
-
--- public/javascripts/build/.module-cache/263d7392a1c8e066490a0721f22c690342504c56.js
+++
@@ -1,5 +0,0 @@
-React.render(
-  React.createElement("h1", null, "Hello, world!"),
-  document.getElementById('example')
-);
-
