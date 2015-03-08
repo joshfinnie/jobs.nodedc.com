1664f37fd05144427640446701aea7d3a56c3f29
Josh Finnie <josh.finnie@trackmaven.com>
2015-03-08 18:49:32 -0400

    adding twitter bootstrap sass and ways to build it.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('js', function() {
     browserify('./public/javascripts/src/app.jsx')
@@ -11,6 +13,14 @@ gulp.task('js', function() {
         .pipe(source('app.js'))
         .pipe(gulp.dest('public/javascripts/build/'));
 });
+
+gulp.task('sass', function() {
+    gulp.src('public/stylesheets/scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public/stylesheets/css'));
+})
 
 gulp.task('watch', function() {
     gulp.watch("public/javascripts/src/**/*.jsx", ["js"])
