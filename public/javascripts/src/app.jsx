d522b239918f172592dfec74f17091649f64d863
Josh Finnie <josh.finnie@trackmaven.com>
2015-03-09 21:54:23 -0400

    fixing bootstraps js with browserify

--- public/javascripts/src/app.jsx
+++ public/javascripts/src/app.jsx
@@ -1,3 +1,5 @@
+var $ = jQuery = require('../../libraries/jquery/dist/jquery');
+var bootstrap = require('../../libraries/bootstrap-sass-official/assets/javascripts/bootstrap');
 var React = require('react');
 var HelloWorld = require('./HelloWorld.jsx');
 
