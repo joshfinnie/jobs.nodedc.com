54b8821cd34ff253ba32ec2bccabd1f0ae4aa719
Josh Finnie <josh.finnie@trackmaven.com>
2015-09-15 14:46:38 -0400

    streamlining the Jobs component some.

--- public/javascripts/src/Jobs.jsx
+++ public/javascripts/src/Jobs.jsx
@@ -4,24 +4,23 @@ var request = require('request');
 var Job = require('./Job.jsx');
 
 module.exports = React.createClass({
+    getInitialState: function() {
+        return {jobs: []}
+    },
+
     componentDidMount: function() {
         request('http://localhost:3000/api/jobs/', function(error, response, body) {
             var result = JSON.parse(body);
             if (this.isMounted()) {
-                this.setState(result);
+                this.setState(result.data);
             }
         }.bind(this));
     },
 
     render: function(){
-        if (this.state == null) {
-            jobs = [];
-        } else {
-            jobs = this.state.data.jobs
-        }
         return (
             <div className="list-group">
-                {jobs.map(function(job){
+                {this.state.jobs.map(function(job){
                     return (
                         <Job
                             key={job.job_id}
